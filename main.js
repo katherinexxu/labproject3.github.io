0079d5273d2d0e5c11abe985ec1e3863e4db146f
katherinexxu <xuk543@newschool.edu>
2021-04-15 18:56:07 -0400

    c

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-var circle = document.getElementById("sun");
-var circle = document.getElementById("moon");
-
-function day() {
-    circle.setAttribute("id", "sun");
-}
-function night() {
-    circle.setAttribute("id", "moon")
+document.getElementById()
