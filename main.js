af098f17c205718116660989ef0ecf0060e9b93e
katherinexxu <xuk543@newschool.edu>
2021-04-15 18:58:44 -0400

    c

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-document.getElementById()
+var day = document.querySelector("button");
+  var night = document.querySelector("night");
+  var body = document.querySelector("body");
+
+function changeDay(event) {
+  if (document.body.classList.contains('night')) {
+    document.body.classList.remove('night');
+    document.body.classList.add('day');
+    console.log("clicked");
+  } else {
+
+    document.body.classList.add('day');
+    console.log("clicked");
+  }
+  
+}
+
+function changeNight(event) {
+  if (document.body.classList.contains('day')) {
+    document.body.classList.remove('day');
+    document.body.classList.add('night');
+    console.log("clicked");
+  } else {
+    document.body.classList.add('night');
+    console.log("clicked");
+  }
+
+}
+             
+day.addEventListener('click', changeDay);
+night.addEventListener('click', changeNight);  
