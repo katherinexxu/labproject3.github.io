4c8d16800dd70045205c328529fad1201e22c31f
katherinexxu <xuk543@newschool.edu>
2021-04-15 18:11:05 -0400

    c

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 var circle = document.getElementById("sun");
-    var sky = document.getElementById("body");
-    var colors = ["#D5F3FE","#343F8A"];
+var circle = document.getElementById("moon");
 
-    function day() {
-        circle.setAttribute("id", "sun");
-        sky.style.backgroundColor = colors[0];
-      }
-      function night() {
-        circle.setAttribute("id", "moon");
-        sky.style.backgroundColor = colors[1];
-      }
+function day() {
+    circle.setAttribute("id", "sun");
+}
+function night() {
+    circle.setAttribute("id", "moon")
