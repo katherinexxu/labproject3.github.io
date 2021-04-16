21602b37532cdfb12ca351db64f5c4a0769b77dd
katherinexxu <xuk543@newschool.edu>
2021-04-15 22:17:31 -0400

    l

--- main.js
+++ main.js
@@ -1,31 +1,73 @@
-var day = document.querySelector("button");
-  var night = document.querySelector("night");
-  var body = document.querySelector("body");
+// var night = document.querySelector('#night-toggle').addEventListener("click", function() {
+//   document.getElementById("sun").innerHTML = "Hello World";
+// });
 
-function changeDay(event) {
-  if (document.body.classList.contains('night')) {
-    document.body.classList.remove('night');
-    document.body.classList.add('day');
-    console.log("clicked");
-  } else {
+// console.log('hi');
 
-    document.body.classList.add('day');
-    console.log("clicked");
-  }
-  
-}
 
-function changeNight(event) {
-  if (document.body.classList.contains('day')) {
-    document.body.classList.remove('day');
-    document.body.classList.add('night');
-    console.log("clicked");
-  } else {
-    document.body.classList.add('night');
-    console.log("clicked");
-  }
+//     // var day = document.querySelector('.day');
+//     // var night = document.querySelector('.night');
+//     // var circle = document.getElementById("sun");
+//     // var sky = document.getElementById("body");
+//     // var colors = ["#D5F3FE","#343F8A"];
 
-}
-             
-day.addEventListener('click', changeDay);
-night.addEventListener('click', changeNight);  
+//     // function today() {
+//     //     circle.css("id", "sun");
+//     //     sky.style.backgroundColor = colors[0];
+//     //   }
+      
+//     // function tonight() {
+//     //     circle.css("id", "moon");
+//     //     sky.style.backgroundColor = colors[1];
+//     //   }
+
+
+//     // day.addEventListener('click', today);
+//     // night.addEventListener('click', tonight);
+
+
+//      var circle = document.getElementById("sun");
+//     var sky = document.getElementById("body");
+//     var colors = ["#D5F3FE","#343F8A"];
+
+//     function day() {
+//         circle.setAttribute("id", "sun");
+//         sky.style.backgroundColor = colors[0];
+//       }
+//       function night() {
+//         circle.setAttribute("id", "moon");
+//         sky.style.backgroundColor = colors[1];
+//       }
+
+var sun = document.querySelector('.sun');
+var moon = document.querySelector('.moon');
+var body = document.querySelector('body');
+var div = document.querySelector('div');
+var clickday = document.querySelector('.clickday');
+var clicknight = document.querySelector('.clicknight');
+ 
+//  function today(){
+//   document.body.css('backgroundColor', '#D5F3FE');
+//   div.classList.remove('moon');
+//   div.classList.add('sun');
+
+//  }
+
+//  function tonight(){
+//   document.body.css('backgroundColor', '#343F8A');
+//   div.classList.remove('sun');
+//   div.classList.add('moon');
+// }
+
+clickday.classList(function(event) {
+  document.body.css('backgroundColor', '#D5F3FE');
+  div.classList.remove('moon');
+  div.classList.add('sun');
+});
+
+
+clicknight.classList(function(event){
+  document.body.css('backgroundColor', '#343F8A');
+  div.classList.remove('sun');
+  div.classList.add('moon');
+});
